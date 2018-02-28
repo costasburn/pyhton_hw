6f9b96ef52670e36e99a0cafb7a2948ec4ab1347
costasburn <burnazaki.1991@gmail.com>
2018-02-28 18:23:05 +0200

    corrected

---
+++ task_10.py
@@ -0,0 +1,20 @@
+data = "Marcus Aurelius*121-04-26*180-03-17"
+
+name, date_born, date_died = data.split("*")
+born_year, born_month, born_day = date_born.split("-")
+died_year, died_month, died_day = date_died.split("-")
+
+new_born_year = int(born_year)
+new_born_month = int(born_month)
+new_born_day = int(born_day)
+new_died_year = int(died_year)
+new_died_month = int(died_month)
+new_died_day = int(died_day)
+
+
+if ((new_died_month - new_born_month) > 0) or ((new_died_month - new_born_month) == 0 and (int(new_died_day - new_born_day) >= 0)):
+    age = new_died_year - new_born_year
+else:
+    age = new_died_year - new_born_year - 1
+
+print(name + ", ", age)
---
+++ task_9.py
@@ -0,0 +1,4 @@
+snake_employee = "employee_first_name"
+print(snake_employee)
+camel_employee = snake_employee.title().replace("_", "")
+print(camel_employee)
