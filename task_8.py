8bad1d89a06aeb3ff8fcad8b644c68d618d9dde1
costasburn <burnazaki.1991@gmail.com>
2018-02-28 17:29:39 +0200

    corrected

--- task_8.py
+++ task_8.py
@@ -1,9 +1,4 @@
-name = str(input("Please enter your first name: "))
-surname = str(input("Please enter your surname: "))
-
-print("Hello, {} {}!".format(name, surname))
-request = input("Would you like to swap your name? Y/N: ")
-if request == "Y":
-    print("Your new name is {} {}".format(surname, name))
-else:
-    print("your name remains unchanged.")
+name = "Konstantin Burnazaki"
+print(name)
+name, surname = name.rsplit(None)
+print(surname + " " + name)
