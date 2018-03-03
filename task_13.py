e35d65211476fdd4542f8076e9e20990c955d792
costasburn <burnazaki.1991@gmail.com>
2018-03-03 20:19:36 +0200

    initial

---
+++ task_13.py
@@ -0,0 +1,16 @@
+import math
+
+def triangle_square_and_perimeter(a, b):
+    area = 0.5 * a * b
+    hypotenuse = math.sqrt(a**2 + b**2)
+    perimetr = a + b + hypotenuse
+    return area, perimetr
+
+
+a = 3
+b = 4
+
+area, perimetr = triangle_square_and_perimeter(a, b)
+
+
+print("Площадь равнобедоенного триугольника с катетами {} и {} равняется {}. А его периметр - {}".format(a, b, area, perimetr))
