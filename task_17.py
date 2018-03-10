cb225670cafb7ebf7066ebbb15c82921b47c7db0
costasburn <burnazaki.1991@gmail.com>
2018-03-10 19:47:43 +0200

    split line No6 into 2 separate lines to improve readability

--- task_17.py
+++ task_17.py
@@ -4,7 +4,8 @@ import math
 def solve_quadratic_equation(a, b, c):
     discriminant = b**2 - 4*a*c
     if discriminant > 0:
-        x1, x2 = (-b + math.sqrt(discriminant)) / (2 * a), (-b - math.sqrt(discriminant)) / (2 * a)
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
     elif discriminant == 0:
         x1 = int(-(b / (2 * a)))
         x2 = None
