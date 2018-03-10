2bda3a95488d58682a8f9d9ed72c9d422d8d138f
costasburn <burnazaki.1991@gmail.com>
2018-03-10 19:26:40 +0200

    corrected if discriminant equals zero case

--- task_17.py
+++ task_17.py
@@ -6,11 +6,11 @@ def solve_quadratic_equation(a, b, c):
     if discriminant > 0:
         x1, x2 = (-b + math.sqrt(discriminant)) / (2 * a), (-b - math.sqrt(discriminant)) / (2 * a)
     elif discriminant == 0:
-        x1 = x2 = (-b + math.sqrt(discriminant))
-        return x1
+        x1 = int(-(b / (2 * a)))
+        x2 = None
     else:
-        x1 = x2 = None
+        x1, x2 = None, None
     return x1, x2
 
 
-print(solve_quadratic_equation(1, -8, 12))
+print(solve_quadratic_equation(1, 2, 1))
