cad0cd6b30f78dbeda3d8b93134b54d7e6541508
costasburn <burnazaki.1991@gmail.com>
2018-03-12 16:42:31 +0200

    corrected as per comments

--- task_23.py
+++ task_23.py
@@ -5,14 +5,14 @@ def guess_check(the_number, guess):
         if guess > the_number:
             guess = int(input("Please, guess lower: "))
             continue
-        if guess < the_number:
+        elif guess < the_number:
             guess = int(input("Please, guess higher: "))
-            continue
     else:
         print("Congrads! You've guessed it!")
 
 
 the_number = random.randint(1, 10)
+print(the_number)
 guess = int(input('Please guess a number 1 to 10: '))
 
 
