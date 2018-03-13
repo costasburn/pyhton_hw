29c71872ca92db8b4b01fc007ab289b10bc82925
costasburn <burnazaki.1991@gmail.com>
2018-03-13 12:46:00 +0200

    corrected

--- task_26.py
+++ task_26.py
@@ -3,11 +3,15 @@ from collections import Counter
 
 
 def calc_frequency(lst):
-    newlist = Counter(lst)
-    if newlist[0] == newlist[1] or newlist[0] == newlist[2] or newlist[1] == newlist[2]:
+    counted_lst = Counter(lst)
+    if counted_lst[0] == counted_lst[1] or counted_lst[0] == counted_lst[2] or counted_lst[1] == counted_lst[2]:
         return None
-    else:
-        return max(lst)
+    elif lst.count(-1) > lst.count(0) and lst.count(-1) > lst.count(1):
+        return -1
+    elif lst.count(0) > lst.count(-1) and lst.count(0) > lst.count(1):
+        return 0
+    elif lst.count(1) > lst.count(-1) and lst.count(1) > lst.count(0):
+        return 1
 
 
 lst = []
