05ff03828d83de10bcbed288d80a5a6db2439d00
costasburn <burnazaki.1991@gmail.com>
2018-03-13 15:53:23 +0200

    task 25 alternative solution

--- task_25.py
+++ task_25.py
@@ -1,12 +1,14 @@
-from random import random
+import random
 
 def shuffle_list(list_to_shuffle):
-    shuffled_list = sorted(list_to_shuffle, key=lambda x: random())
-    print(shuffled_list)
+    for i in range(len(list_to_shuffle)):
+        k = random.randint(0, len(list_to_shuffle)-1)
+        list_to_shuffle[k] = list_to_shuffle[i]
+    return list_to_shuffle
 
 list_to_shuffle = []
 for i in range(0, 100):
     if i % 2 != 0:
         list_to_shuffle.append(i)
 
-shuffle_list(list_to_shuffle)
+print(shuffle_list(list_to_shuffle))
