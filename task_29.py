c9ccaf87ee47c751060bc2953892682817b56cea
costasburn <burnazaki.1991@gmail.com>
2018-03-19 15:47:13 +0200

    task_29 / quotas implemented

--- task_29.py
+++ task_29.py
@@ -7,25 +7,34 @@ def gen_password():
     encrypt_list_symbols = '_'
     password_length = 8
     password = ''
-    quantity_letter_symbols = 0
-    reserve_for_title_and_digits = 2
-    while quantity_letter_symbols < 1 or password_length - quantity_letter_symbols < reserve_for_title_and_digits:
-        quantity_letter_symbols = round(random.random() * password_length)
-    print("Quantity of letter symbols: ", quantity_letter_symbols)
-    quantity_title_symbols = 0
-    while quantity_title_symbols < 1 or password_length - quantity_title_symbols - quantity_letter_symbols == 0:
-        quantity_title_symbols = round(random.random() * (password_length - quantity_letter_symbols))
-    print("Quantity of title symbols: ", quantity_title_symbols)
-    quantity_digits_symbols = 0
-    while quantity_digits_symbols < 1:
-        quantity_digits_symbols = math.ceil(
-            random.random() * (password_length - quantity_title_symbols - quantity_letter_symbols))
-    print("Quantity of digits symbols: ", quantity_digits_symbols)
-    quantity_symbol = max(0,
-                          password_length - quantity_letter_symbols - quantity_title_symbols - quantity_digits_symbols)
-    print("Quantity of symbols: ", quantity_symbol)
+    quotas = [1, 1, 1, 1]
+    password_length -= sum(quotas)
 
-    quotas = [quantity_letter_symbols, quantity_title_symbols, quantity_digits_symbols, quantity_symbol]
+    while password_length > 0:
+        rand_idx = random.randint(0, len(quotas) - 1)
+        quotas[rand_idx] += 1
+        password_length -= 1
+    # quantity_letter_symbols = 0
+    # reserve_for_title_and_digits = 2
+    # while quantity_letter_symbols < 1 or password_length - quantity_letter_symbols < reserve_for_title_and_digits:
+    #     quantity_letter_symbols = round(random.random() * password_length)
+    # print("Quantity of letter symbols: ", quantity_letter_symbols)
+    # quantity_title_symbols = 0
+    # while quantity_title_symbols < 1 or password_length - quantity_title_symbols - quantity_letter_symbols == 0:
+    #     quantity_title_symbols = round(random.random() * (password_length - quantity_letter_symbols))
+    # print("Quantity of title symbols: ", quantity_title_symbols)
+    # quantity_digits_symbols = 0
+    # while quantity_digits_symbols < 1:
+    #     quantity_digits_symbols = math.ceil(
+    #         random.random() * (password_length - quantity_title_symbols - quantity_letter_symbols))
+    # print("Quantity of digits symbols: ", quantity_digits_symbols)
+    # quantity_symbol = max(0,
+    #                       password_length - quantity_letter_symbols - quantity_title_symbols - quantity_digits_symbols)
+    # print("Quantity of symbols: ", quantity_symbol)
+    # quantity_symbol = 0
+
+
+    # quotas = [quantity_letter_symbols, quantity_title_symbols, quantity_digits_symbols, quantity_symbol]
     symb_sources = [ascii_lowercase, ascii_uppercase, digits, encrypt_list_symbols]
 
     for i, quota in enumerate(quotas):
