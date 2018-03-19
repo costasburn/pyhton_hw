eb557945e52c38a233f9f3b7f1392494eddc697f
costasburn <burnazaki.1991@gmail.com>
2018-03-19 15:17:34 +0200

    task_29 / corrected as per comments, implemented enumerate

--- task_30.py
+++ task_30.py
@@ -1,8 +1,8 @@
 def gen_primes(lower_limit, upper_limit):
     prime_numbers = []
     for i in range(lower_limit, upper_limit + 1):
-        for j in range(1, i):
-            if i % j == 0 and j != 1:
+        for j in range(2, i):
+            if i % j == 0 and j == i // 2:
                 break
             if j == i - 1:
                 prime_numbers += [i]
