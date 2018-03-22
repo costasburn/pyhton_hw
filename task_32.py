15e20d9c10becbaeeb44e8178103d61475ba457c
costasburn <burnazaki.1991@gmail.com>
2018-03-22 16:19:41 +0200

    task_32 / initial

--- task_32.py
+++ task_32.py
@@ -1,41 +1,65 @@
 import csv
+titanic = r'C:\Users\Konstantin\IdeaProjects\pyhton_hw\titanic_data.csv'
 
-titanic_data = """1,0,3,"Braund, Mr. Owen Harris",male,22,1,0,A/5 21171,7.25,,S
-2,1,1,"Cumings, Mrs. John Bradley (Florence Briggs Thayer)",female,38,1,0,PC 17599,71.2833,C85,C
-3,1,3,"Heikkinen, Miss. Laina",female,26,0,0,STON/O2. 3101282,7.925,,S
-4,1,1,"Futrelle, Mrs. Jacques Heath (Lily May Peel)",female,35,1,0,113803,53.1,C123,S
-5,0,3,"Allen, Mr. William Henry",male,35,0,0,373450,8.05,,S
-6,0,3,"Moran, Mr. James",male,,0,0,330877,8.4583,,Q
-7,0,1,"McCarthy, Mr. Timothy J",male,54,0,0,17463,51.8625,E46,S
-8,0,3,"Palsson, Master. Gosta Leonard",male,2,3,1,349909,21.075,,S
-9,1,3,"Johnson, Mrs. Oscar W (Elisabeth Vilhelmina Berg)",female,27,0,2,347742,11.1333,,S
-10,1,2,"Nasser, Mrs. Nicholas (Adele Achem)",female,14,1,0,237736,30.0708,,C
-11,1,3,"Sandstrom, Miss. Marguerite Rut",female,4,1,1,PP 9549,16.7,G6,S
-12,1,1,"Bonnell, Miss. Elizabeth",female,58,0,0,113783,26.55,C103,S
-13,0,3,"Saundercock, Mr. William Henry",male,20,0,0,A/5. 2151,8.05,,S
-14,0,3,"Andersson, Mr. Anders Johan",male,39,1,5,347082,31.275,,S
-15,0,3,"Vestrom, Miss. Hulda Amanda Adolfina",female,14,0,0,350406,7.8542,,S
-16,1,2,"Hewlett, Mrs. (Mary D Kingcome) ",female,55,0,0,248706,16,,S
-17,0,3,"Rice, Master. Eugene",male,2,4,1,382652,29.125,,Q
-18,1,2,"Williams, Mr. Charles Eugene",male,,0,0,244373,13,,S"""
+def get_data_from_csv(path):
+    with open(path, "r") as f:
+        list_dicts = [row for row in csv.DictReader(f)]
+    return list_dicts
 
-# PassengerId = []
-# Survived = []
-# Pclass = []
-# Name = []
-# Sex = []
-# Age = []
-# SibSp = []
-# Parch = []
-# Ticket = []
-# Fare = []
-# Cabin = []
-# Embarked = []
-# columns = [PassengerId, Survived, Pclass, Name, Sex, Age, SibSp, Parch, Ticket, Fare, Cabin, Embarked]
 
-def csv_reader(file_obj):
-    reader = csv.reader(file_obj)
-    for item in reader:
-        print(item)
+titanic_data = get_data_from_csv(titanic)
 
-csv_reader(titanic_data)
+survived_list = []
+survived_m = []
+survived_w = []
+survived_1class = []
+survived_2class = []
+survived_3class = []
+
+total_m = []
+total_w = []
+total_1class = []
+total_2class = []
+total_3class = []
+
+for passenger in titanic_data:
+    if passenger["Survived"] == "1":
+        survived_list.append(passenger)
+for passenger in survived_list:
+    if passenger["Sex"] == "male":
+        survived_m.append(passenger)
+    if passenger["Sex"] == "female":
+        survived_w.append(passenger)
+    if passenger["Pclass"] == "1":
+        survived_1class.append(passenger)
+    if passenger["Pclass"] == "2":
+        survived_2class.append(passenger)
+    elif passenger["Pclass"] == "3":
+        survived_3class.append(passenger)
+
+for passenger in titanic_data:
+    if passenger["Sex"] == "male":
+        total_m.append(passenger)
+    if passenger["Sex"] == "female":
+        total_w.append(passenger)
+    if passenger['Pclass'] == "1":
+        total_1class.append(passenger)
+    if passenger["Pclass"] == "2":
+        total_2class.append(passenger)
+    elif passenger["Pclass"] == "3":
+        total_3class.append(passenger)
+
+men_survive_rate = (len(survived_m) / len(total_m)) * 100
+print("{}% of men survived".format(men_survive_rate))
+
+wmen_survive_rate = (len(survived_w) / len(total_w)) * 100
+print("{}% of women survived".format(wmen_survive_rate))
+
+first_class_survive_rate = (len(survived_1class) / len(total_1class) * 100)
+print("{}% of first class survived".format(first_class_survive_rate))
+
+second_class_survive_rate = (len(survived_2class) / len(total_2class) * 100)
+print("{}% of second class survived".format(second_class_survive_rate))
+
+third_class_survive_rate = (len(survived_3class) / len(total_3class) * 100)
+print("{}% of third class survived".format(third_class_survive_rate))
