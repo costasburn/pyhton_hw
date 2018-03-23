4b6ce59652c4aece01297d5244a03c1f2b75a0b4
costasburn <burnazaki.1991@gmail.com>
2018-03-23 19:29:51 +0200

    task_32 / formatted

--- task_32.py
+++ task_32.py
@@ -1,25 +1,7 @@
 import csv
 
-# persons = [
-#     {'name': 'Alice', 'age': 23, 'gender': 'f'},
-#     {'name': 'Bob', 'age': 22, 'gender': 'm'},
-#     {'name': 'Stacy', 'age': 33, 'gender': 'f'},
-#     {'name': 'Clark', 'age': 43, 'gender': 'm'},
-#     {'name': 'Bill', 'age': 42, 'gender': 'm'}]
-#
-# totals = {}
-# field = 'gender'
-# for person in persons:
-#     totals[person[field]] = totals.get(person[field], 0) + 1
-#
-#
-# print('Gender distribution:')
-# for k, v in totals.items():
-#     print('%s: %.2f%%' % (k, v / len(persons) * 100))
-
-
-
 titanic = r'C:\Users\Konstantin\IdeaProjects\pyhton_hw\titanic_data.csv'
+
 
 def get_data_from_csv(path):
     with open(path, "r") as f:
@@ -40,15 +22,8 @@ def distribution_of_survivals(titanic_data, field_name):
             survived[person[field_name]] = survived.get(person[field_name], 0) + 1
 
     for key_survived, value_survived in survived.items():
-        print("Survival rate in %s %s: %.2f%%" % (field_name, key_survived, value_survived / totals[key_survived] * 100))
-    # for key_total, value_total in totals.items():
-    #     for key_survived, value_survived in survived.items():
-    #         if key_total == key_survived:
-    #             print("Survival rate in %s %s: %.2f%%" % (field_name, key_total, value_survived / value_total * 100))
-
-
-
-
+        print(
+            "Survival rate in %s %s: %.2f%%" % (field_name, key_survived, value_survived / totals[key_survived] * 100))
 
 
 field_name = "Pclass"
