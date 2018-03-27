781a74a6c41b5985889c7a0d2f3c54b100b4163b
costasburn <burnazaki.1991@gmail.com>
2018-03-27 16:59:23 +0300

    8 points tasks / corrected as per comments

--- 8_points_task.py
+++ 8_points_task.py
@@ -18,26 +18,26 @@ def print_matrix(matrix):
             print("%3d" % (elem), end=" ")
     print()
 
-def find_base_elem(matrix):
+def find_saddle_elem(matrix):
     print()
-    base_elem_coord_list = []
+    saddle_elem_coord_list = []
     for i in range(len(matrix)):
-        base_elem_coord = []
+        saddle_elem_coord = []
         min_val = min(matrix[i])
         index_min_val = matrix[i].index(min_val)
         column_list = []
         for j in range(len(matrix)):
             column_list.append(matrix[j][index_min_val])
-        if min_val is max(column_list):
-            base_elem_coord.append(i)
-            base_elem_coord.append(matrix[i].index(min_val))
-            base_elem_coord_list.append(base_elem_coord)
-    return base_elem_coord_list
+        if min_val == max(column_list):
+            saddle_elem_coord.append(i)
+            saddle_elem_coord.append(matrix[i].index(min_val))
+            saddle_elem_coord_list.append(saddle_elem_coord)
+    return saddle_elem_coord_list
 
 
 new_matrix = generate_matrix(3, 3, -5, 5)
 print_matrix(new_matrix)
-print(find_base_elem(new_matrix))
+print(find_saddle_elem(new_matrix))
 
 print("*" * 20)
 print("Task 11")
@@ -52,9 +52,9 @@ def sort_matrix(matrix):
         for row in range(len(matrix)):
             list_of_column_elems.append(matrix[row][column])
         if column % 2 == 0:
-            list_of_column_elems = sorted(list_of_column_elems)
+            list_of_column_elems.sort()
         else:
-            list_of_column_elems = sorted(list_of_column_elems, reverse=True)
+            list_of_column_elems.sort(reverse=True)
         for row in range(len(matrix)):
             matrix[row][column] = list_of_column_elems[row]
     print()
