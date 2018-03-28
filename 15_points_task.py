90be3bf30465c4b39b02ffa4bf90694844c3fed9
costasburn <burnazaki.1991@gmail.com>
2018-03-28 15:37:39 +0300

    15 points tasks

--- 15_points_task.py
+++ 15_points_task.py
@@ -6,20 +6,15 @@ def after_summer_test(quantity_of_problems_to_generate):
     # generating multiplication table
     for x in range(2, 10):
         for y in range(2, 10):
-            temp_list.append([x, y])
-    # removing duplicates from generated table
-    for problem in temp_list:
-        for problem_to_compare in temp_list:
-            if (problem[0], problem[1]) == (problem_to_compare[1], problem_to_compare[0]) and problem[0] != problem[1]:
-                temp_list.remove(problem)
+            if (y, x) not in temp_list:
+                temp_list.append((x, y))
     # choosing 15 random problems from the table
     for _ in range(quantity_of_problems_to_generate):
         result_list.append(random.choice(temp_list))
     # printing out
-    for problem in result_list:
-        print("{} x {} = ?".format(problem[0], problem[1]))
+    for i, problem in enumerate(result_list):
+        print("{}) {} x {} = ?".format(i + 1, problem[0], problem[1]))
     return result_list
-
 
 
 after_summer_test(15)
