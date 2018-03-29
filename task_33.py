6dc7837ef5df5ba9a8020d1162e6e507128dba54
costasburn <burnazaki.1991@gmail.com>
2018-03-29 15:21:00 +0300

    task 33 / corrected as per comments

--- task_33.py
+++ task_33.py
@@ -8,19 +8,14 @@ class Godzilla:
     def eat(self, humans_weight):
         if self.is_full():
             print("Already full")
-            return self.stomach_remaining_space
         elif humans_weight >= self.stomach_remaining_space:
             print("Human is too heavy")
-            return self.stomach_remaining_space
         else:
             self.stomach_remaining_space -= humans_weight
-            return self.stomach_remaining_space
+        return self.stomach_remaining_space
 
     def is_full(self):
-        if self.stomach_remaining_space <= self.STOMACH_FILL_LIMIT:
-            return True
-        else:
-            return False
+        return self.stomach_remaining_space <= self.STOMACH_FILL_LIMIT
 
 
 Kesha = Godzilla()
@@ -28,4 +23,3 @@ print(Kesha.eat(50))
 print(Kesha.eat(45))
 print(Kesha.eat(1))
 print(Kesha.is_full())
-
